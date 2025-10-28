a195eafdbc3bc70506337f31cf3548fec653cafb
Gloria <mcalping@ccsdbraves.org>
2025-10-28 17:46:39 -0400

    Got rid of camera and old stuff from 2023 and 2024 in constants

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,7 +4,6 @@
 
 package frc.robot;
 
-import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -20,7 +19,7 @@ public class Robot extends TimedRobot {
   public void robotInit() {
     System.out.println("Robot Init Called");
     new RobotContainer();
-    CameraServer.startAutomaticCapture();
+ 
     //Autos.init();
      
   }
